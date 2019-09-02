bfc5ac0926e421d62b127ade0861c2a639d1c034
TioaTyan <xiaozhidiguo@gmail.com>
2019-09-02 14:49:25 +0800

    按作者遍历

--- test.h
+++ test.h
@@ -6,12 +6,14 @@ void searchbyauthor(LNode* L) {
 	LinkList searchresult = new LNode;
 	searchresult->next = NULL;//初始化搜索结果链表
 	LNode* p1 = L->next;//指向数据源的指针
-	LNode* p2 = searchresult->next;//指向结果集的指针
+	LNode* p2 = searchresult;//指向结果集的指针
 	while (p1)//遍历数据源
 	{
 		if (strcmp(p1->data.author, keywords) == 0) {//如果作者相同
-			p2->data = p1->data; //数据源数据赋给结果集
-			p2 = p2->next;//
+			LNode* q = new LNode;
+			q->data=p1->data;//数据源数据赋给结果集
+			q->next=p2->next;
+			p2->next=q;
 		}
 		p1 = p1->next;
 	}
