595a1a0ff01d989a503e665204cb6da988c681c3
TioaTyan <xiaozhidiguo@gmail.com>
2019-09-02 22:01:27 +0800

    make source file great again

--- adding.h
+++ adding.h
@@ -1,10 +1,10 @@
 ﻿#pragma once
 Status AddNewBook(LNode* L) {
 	float price;
-	int flag; // 检测no
+	int flag; // 检测No
 	LNode* p = L->next;
 	char name[50], au[50], pub[50], no[50];
-	cout << "请分别输入书号、书名、作者（1个）、出版社、价格五个部分" << endl;
+	cout << "请分别输入书号、书名、作者（1个）、出版社、价格五个部分,以空格隔开：" << endl;
 	int length = ListLength(L);
 	cin >> no >> name >> au >> pub >> price;
 	while (1) {
